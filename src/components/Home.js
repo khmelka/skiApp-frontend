ea6deedbe2b49e17e42bfb0ecc592cc9a48050e3
Nelly <nkhmeleva@yahoo.com>
2019-09-05 17:05:58 -0600

    added pins

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Map from './Map'
+import Map from './Map';
 
 export default class Home extends Component {
     render() {
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
 import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps'
 
+import * as tennisCourts from './tennisCourtsJSON.json'
+
 
 function GMap() {
-        return (
-            <GoogleMap defaultZoom={10} defaultCenter={{lat: 39.739235, lng: -104.990250 }} />
+            return (
+                <GoogleMap defaultZoom={12} defaultCenter={{lat: 39.739235, lng: -104.990250 }} 
+            >
+                
+                {tennisCourts.tennisCourts.map((court) => (
+                    <Marker 
+                      key={court.name} 
+                      position={{
+                          lat: court.Latitude,
+                          lng: court.Longitude
+
+                      }}
+                    />
+                ))}
+
+                </GoogleMap>
         )
     }
 
@@ -13,13 +29,11 @@ const WrappedMap = withScriptjs(withGoogleMap(GMap))
 
 export default function Map() {
     return (
+       
 
         <div className="map">
-             <div>
-                <h1>Hello User</h1>
-                <h1>Please select available car</h1>
-            </div>
-            <div>
+           <h1>HI</h1>
+            
                 <WrappedMap
                     googleMapURL={'https://maps.googleapis.com/maps/api/js?key=AIzaSyD8Jr8yyjkIQ2PUnGxroY_g9MyLVFVqr0o&v=3.exp&libraries=geometry,drawing,places'} 
                     loadingElement={<div style={{height: "100%"}} />}
@@ -27,7 +41,8 @@ export default function Map() {
                     containerElement={<div className="style" style={{ }} />}
                     mapElement={<div style={{ height: `100%` }} />}
                 />
-            </div>
+           
         </div>
     )
 }
+
